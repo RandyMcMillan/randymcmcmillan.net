481cc576a7d2c2e8d77c07db2bd0e199cd51e076
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-06-30 13:56:51 -0400

    update from randymcmillan on 1625075811

--- install-depends.sh
+++ install-depends.sh
@@ -73,9 +73,8 @@ if [[ "$OSTYPE" == "linux"* ]]; then
             curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
             sudo apt install ./keybase_amd64.deb
             run_keybase
-            pip3 install blockcypher twitterscraper
-            pip3 install setuptools
             sudo apt install  libappindicator1 libdbusmenu-gtk4 libindicator7 indicator-application python-setuptools #keybase
+            pip3 install blockcypher twitterscraper
         fi
     fi
     if [[ "$OSTYPE" == "linux-musl" ]]; then
